adfe7275850fc1925d397c423de332ae59692918
sammybao <bvo@umass.edu>
2016-11-13 23:11:58 -0500

    Update Driver_main.php

--- Driver_main.php
+++ Driver_main.php
@@ -41,7 +41,7 @@
             }
             ?>
             </h3> 
-            <h4>Enter the ID of the order </h4>
+            <h2>Enter the ID of the order </h2>
             <form action="driver_fetch.php" method="get">
                 <input type="hidden" name="name_ID" value="$uname">
                 ID <input type="text" name="listID"><br>

93b223266ec4f0a74abab097598a3cfcbbe3cf19
Haoxian Lin <haoxianlin@umass.edu>
2016-12-14 03:02:01 +0000

    submitted

--- addupdel.php
+++ addupdel.php
@@ -13,14 +13,14 @@ $quan=(int)$quan;
 $newItem=$_POST["item"];
 $newItem=(string)$newItem;
 
-$submited=$_POST["listSubmited"];//it is 1 if  we are adding from Client_main_update_order.php
+$submitted=$_POST["listSubmitted"];//it is 1 if  we are adding from Client_main_update_order.php
 
 $flag=$_POST["adding"];
 $flag=(int)$flag;
 
 
 //getting the new items that is going to be added
-//if($flag==1){ //adding items
+if($flag==1){ //adding items
 	//$pos = strpos($items,$newItem);
 	//$pos=(int)$pos;
 	//echo "$newItem<br>";
@@ -40,27 +40,27 @@ $flag=(int)$flag;
 		$items .= "$newItem  $quan";
 		$_SESSION["listItem"]=$items;
 		$newvar=$_SESSION["listItem"];
-		if ($newItem != "" && $quan > 0 && $submited!="1")header("Location:./Client_main.php");
-		elseif ($newItem != "" && $quan > 0 && $submited=="1")header("Location:./Client_main_update_order.php");
+		if ($newItem != "" && $quan > 0 && $submitted!="1")header("Location:./Client_main.php");
+		elseif ($newItem != "" && $quan > 0 && $submitted=="1")header("Location:./Client_main_update_order.php");
 
 	}
 	elseif ($newItem != "" && $quan > 0){
 		$items .= ","." $newItem  $quan";
 		$_SESSION["listItem"]=$items;
 		$newvar=$_SESSION["listItem"];
-		if ($newItem != "" && $quan > 0 && $submited!="1")header("Location:./Client_main.php");
-		elseif ($newItem != "" && $quan > 0 && $submited=="1")header("Location:./Client_main_update_order.php");
+		if ($newItem != "" && $quan > 0 && $submitted!="1")header("Location:./Client_main.php");
+		elseif ($newItem != "" && $quan > 0 && $submitted=="1")header("Location:./Client_main_update_order.php");
 
 
 	}
-	elseif ($quan<0){
+	elseif ($quan<0 && $submitted=="0"){
 	//can send a flag saying that  an incorrect value was entered
 		//NEED TO CHECK WHERE WE NEED TO REDIRECT TO
-		if ($submited=="0")header("Location:./Client_main.php?flag=1");
-        	elseif ($submited=="1")header("Location:./Client_main_update_order.php?flag=1");
+		header("Location:./Client_main.php?flag=1");
+	}
+        elseif ($quan<0 && $submitted=="1"){header("Location:./Client_main_update_order.php?flag=1");}
 
 		//header("Location:./Client_main.php?flag=1");
-	}
 	//echo "the concatenation is: $items <br>";
 	
 	//checking if that item had already been added
@@ -71,11 +71,11 @@ $flag=(int)$flag;
 //	$newvar=$_SESSION["listItem"];
 
 	//echo "the session variable is now: $newvar";
-//	if ($newItem != "" && $quan > 0 && $submited!="1")header("Location:./Client_main.php");
-//	if ($newItem != "" && $quan > 0 && $submited=="1")header("Location:./Client_main_update_order.php");
-//}
+//	if ($newItem != "" && $quan > 0 && $submitted!="1")header("Location:./Client_main.php");
+//	if ($newItem != "" && $quan > 0 && $submitted=="1")header("Location:./Client_main_update_order.php");
+}
 
-/*
+
 elseif($flag==2){ //delete
 	//first we check if the item existed or not in the current shopping list
 	$pos = strpos($items,$newItem); //will be false if not found
@@ -93,7 +93,7 @@ elseif($flag==2){ //delete
 	}
 	
 }
-*/
+
 ?>
 
 

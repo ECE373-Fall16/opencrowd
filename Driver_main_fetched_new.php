499dd54e9a8242ec653e388ca60c70900f646696
Haoxian Lin <haoxianlin@umass.edu>
2016-12-14 20:18:18 +0000

    travis back

--- Driver_main_fetched_new.php
+++ Driver_main_fetched_new.php
@@ -7,6 +7,7 @@ session_start();
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="robots" content="index,follow">
+	<meta http-equiv="refresh" content="5">
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 

207de5d6a74de1309b7afa3f2d3813df2a66977c
haoxianlin <haoxianlin@umass.edu>
2016-12-13 22:38:58 -0500

    forcing change to tester.php

--- testing/tester.php
+++ testing/tester.php
@@ -35,24 +35,14 @@ $_POST["answer"]='hi man i fuk you';
 
 include 'client_register.php';
 
-$returned_set = $db->querySingle("SELECT * FROM client WHERE USERNAME='cat';");
-$entry = $returned_set->fetcharray();
+ $db = new MyDB();
 
+$drivret = $db->querySingle("SELECT COUNT(*) FROM clients WHERE USERNAME='$uname' AND PASSWORD='$pass';");
+        if($drivret!=1){ //neither client nor driver
+                $db->close();
+                exit(1);
 
-
-
-
-
-
-/*
-$db = new MyDB1();
- $sqldisplay =<<<EOF
-      select * from clients;
-EOF;
-
-$ret = $db->exec($sqldisplay);
-echo $ret;
+}
 $db->close();
-*/
-
+?>
 ?>

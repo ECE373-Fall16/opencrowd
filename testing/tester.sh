6132e4b2e511b7ab2e8dd33732087e2400f78cf6
Haoxian Lin <haoxianlin@umass.edu>
2016-12-15 00:17:12 +0000

    added list noitem list testing

--- testing/tester.sh
+++ testing/tester.sh
@@ -5,3 +5,4 @@ php testing/cases/login_simple_client.php
 php testing/cases/login_simple_driver.php
 php testing/cases/login_wrongpass.php
 php testing/cases/list_submit.php
+php testing/cases/list_noitem_submit.php

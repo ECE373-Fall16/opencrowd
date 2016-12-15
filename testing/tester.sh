a97d8e44ac06c9da012a8f7660be6910e110991d
Haoxian Lin <haoxianlin@umass.edu>
2016-12-15 02:16:22 +0000

    comlete list work float test added

--- testing/tester.sh
+++ testing/tester.sh
@@ -8,4 +8,4 @@ php testing/cases/list_submit.php
 php testing/cases/list_noitem_submit.php
 php testing/cases/list_delete.php
 php testing/cases/list_fetch.php
-testing/cases/list_complete.php
+php testing/cases/list_complete.php
